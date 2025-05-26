3af18a8c189fc53b07efa43cacb8d736279bf032
Pandoks_ <35944715+Pandoks@users.noreply.github.com>
2025-05-26 01:01:45 -0700

    update: svelteplex pages

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# tzugi
+# SveltePlex
--- packages/svelte/src/routes/+page.svelte
+++ packages/svelte/src/routes/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-  import { enhance } from '$app/forms';
   import { Badge } from '@lib/components/ui/badge';
-  import { Input } from '@lib/components/ui/input';
-  import { Button } from '@lib/components/ui/button';
 </script>
 
 <div class="flex h-screen w-screen flex-col items-center justify-center">
@@ -16,9 +13,4 @@
     <h1 class="font-garamond -mt-2 text-9xl font-medium">SveltePlex</h1>
     <h2 class="font-garamond mt-5 text-lg">Monorepo for Everything Svelte</h2>
   </div>
-
-  <form class="flex" use:enhance>
-    <Input placeholder="" id="email" type="email" />
-    <Button type="submit">Submit</Button>
-  </form>
 </div>
